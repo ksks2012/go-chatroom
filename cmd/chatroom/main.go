e0a5f12ba1e063fb25d87f89d5c562c34d44a465
ksks2012 <st890751999@gmail.com>
2021-12-19 22:27:13 +0800

    feat(http handle): add basic handle

---
+++ cmd/chatroom/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/go-chatroom/server"
+)
+
+var (
+	addr   = ":2022"
+	banner = `
+    ____              _____
+   |    |    |   /\     |
+   |    |____|  /  \    |
+   |    |    | /----\   |
+   |____|    |/      \  |
+
+GChat Room：%s
+`
+)
+
+func main() {
+	fmt.Printf(banner+"\n", addr)
+
+	server.RegisterHandle()
+
+	log.Fatal(http.ListenAndServe(addr, nil))
+}
